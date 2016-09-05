2e6dbe9784038a04fbfabae7bfbc3e316ec2be7a
SethuKapa <s214258297@nmmu.ac.za>
2016-09-05 10:27:11 +0200

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WRR301_Project.Models
+{
+    public class Product
+    {
+        public char ProductID {get; set; }
+        public string ProdDescription { get; set; }
+        public double Price { get; set; }
+        public string ShopName { get; set; }
+        public char OnSale { get; set; }
+    }
+
+    public class ProductDBContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ SearchController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace WRR301_Project.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index(string searchString)
+        {
+            var products = from Product in 
+                         select Product;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                products = products.Where(s => s.Title.Contains(searchString));
+            }
+
+            return View(products);
+        }
+    }
+}
